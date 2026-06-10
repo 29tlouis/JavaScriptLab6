6264f3ac1a1e314eef1878e80fd409c6b51d718b
29tlouis <29tlouis@cpsd.us>
2026-06-10 13:35:48 -0400

    Added the console log greeting

--- js/02_string_concatenation.js
+++ js/02_string_concatenation.js
@@ -1,10 +1,10 @@
 document.write("02_string_concatenation.js is linked");
 //1. Type your first name and last name into the variables below between the quotation marks
-let firstName = "";
-let lastName = "";
+let firstName = "Tevin";
+let lastName = "Louis";
 
 //2.  Use a console log statement to print out a greeting to the console that says "Hello,"" 
 // followed by your first name and last name.  i.e. Hello Jane Smith.  Make sure you add a space 
 // between the firstName and lastName
-
+console.log("Hello, " + firstName + " " + lastName);
  
